af160956a8c19b7786fbee26d88376cd3e262638
Tushar K Rao <tusharkrao999@gmail.com>
2023-05-12 18:59:38 -0700

    Login page commit phase 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import './App.css';
+//
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
+
+
+import React from 'react';
+
 import './App.css';
 
+import Dashboard from './components/Dashboard/Dashboard';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Preferences from './components/Preferences/Preferences';
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Dashboard/>
+    <Preferences/>
+    </>
+
   );
+
 }
 
 export default App;
