e64de13526932d95a2a2a677e135ad20ac6808f1
Tushar K Rao <tusharkrao999@gmail.com>
2023-05-13 21:17:36 -0700

    Code template to check for user state change added

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,3 +19,21 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+
+
+//to check whether the user state changes after
+//import { getAuth, onAuthStateChanged } from "firebase/auth";
+//
+//const auth = getAuth();
+//onAuthStateChanged(auth, (user) => {
+//  if (user) {
+//    // User is signed in, see docs for a list of available properties
+//    // https://firebase.google.com/docs/reference/js/firebase.User
+//    const uid = user.uid;
+//    // ...
+//  } else {
+//    // User is signed out
+//    // ...
+//  }
+//});
