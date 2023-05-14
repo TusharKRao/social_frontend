ca007740119d61a8d81206b12fbf59b24a638471
Tushar K Rao <tusharkrao999@gmail.com>
2023-05-13 19:01:35 -0700

    Login auth with firebase initial commit

---
+++ frontend/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs8demIG8sSY5UwElanLBIcMrVwZXfusg",
+  authDomain: "react-auth-26a06.firebaseapp.com",
+  projectId: "react-auth-26a06",
+  storageBucket: "react-auth-26a06.appspot.com",
+  messagingSenderId: "257524598496",
+  appId: "1:257524598496:web:c70c975abb71d56680b285"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
