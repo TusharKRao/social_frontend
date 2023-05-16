7d8f525e29349887d2eb07a571aaffbd8a4ffa40
Tushar K Rao <tusharkrao999@gmail.com>
2023-05-16 12:58:08 -0700

    Demo design found, rework in progress

--- frontend/src/MainPageComponents/MainPage.js
+++ frontend/src/MainPageComponents/MainPage.js
@@ -1,64 +1,89 @@
-import React, {useEffect, useState} from 'react';
-import { Navigate } from 'react-router-dom';
-import {useLocation} from 'react-router-dom';
+import React, { useState } from "react";
 
-import React from 'react';
+//import react pro sidebar components
 import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink,
-} from './NavbarElements';
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
 
-export const MainPage = (props) => {
-
-    const [authenticated, setAuthenticated] = useState(null);
-    const location = useLocation();
-    console.log(location.state.test);
-    useEffect(() => {
-        const loggedInUser = localStorage.getItem("authenticated");
-        if (loggedInUser) {
-            setAuthenticated(loggedInUser);
-        }
-    }, []);
+//import icons from react icons
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
 
 
-    return(
-              <>
-                <Nav>
-                  <Bars />
-
-                  <NavMenu>
-                    <NavLink to='/about' activeStyle>
-                      About
-                    </NavLink>
-                    <NavLink to='/events' activeStyle>
-                      Events
-                    </NavLink>
-                    <NavLink to='/annual' activeStyle>
-                      Annual Report
-                    </NavLink>
-                    <NavLink to='/team' activeStyle>
-                      Teams
-                    </NavLink>
-                    <NavLink to='/blogs' activeStyle>
-                      Blogs
-                    </NavLink>
-                    <NavLink to='/sign-up' activeStyle>
-                      Sign Up
-                    </NavLink>
-                    {/* Second Nav */}
-                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-                  </NavMenu>
-                  <NavBtn>
-                    <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-                  </NavBtn>
-                </Nav>
-              </>
-            );
+//import sidebar css from react-pro-sidebar module and our custom css
+import "react-pro-sidebar/dist/css/styles.css";
+import "./MainPage.css";
 
 
-}
+export const MainPage = () => {
+
+    //create initial menuCollapse state using useState hook
+    const [menuCollapse, setMenuCollapse] = useState(false)
+
+    //create a custom function that will change menucollapse state from false to true and true to false
+  const menuIconClick = () => {
+    //condition checking to change state from true to false and vice versa
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+  };
+
+  return (
+    <>
+      <div id="header">
+          {/* collapsed props to change menu size using menucollapse state */}
+        <ProSidebar collapsed={menuCollapse}>
+          <SidebarHeader>
+          <div className="logotext">
+              {/* small and big change using menucollapse state */}
+              <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
+            </div>
+            <div className="closemenu" onClick={menuIconClick}>
+                {/* changing menu collapse icon on click */}
+              {menuCollapse ? (
+                <FiArrowRightCircle/>
+              ) : (
+                <FiArrowLeftCircle/>
+              )}
+            </div>
+          </SidebarHeader>
+          <SidebarContent>
+            <Menu iconShape="square">
+              <MenuItem active={true} icon={<FiHome />}>
+                Home
+              </MenuItem>
+              <MenuItem icon={<FaList />}>Category</MenuItem>
+              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
+              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
+              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+            </Menu>
+          </SidebarContent>
+          <SidebarFooter>
+            <Menu iconShape="square">
+              <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+            </Menu>
+          </SidebarFooter>
+        </ProSidebar>
+      </div>
+    </>
+  );
+};
+
+
+
+//    const [authenticated, setAuthenticated] = useState(null);
+//    const location = useLocation();
+//    console.log(location.state.test);
+//    useEffect(() => {
+//        const loggedInUser = localStorage.getItem("authenticated");
+//        if (loggedInUser) {
+//            setAuthenticated(loggedInUser);
+//        }
+//    }, []);
+
 
