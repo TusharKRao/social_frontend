f6f0948ef41cd49a21eec2ce89898eab0ed89116
Tushar K Rao <tusharkrao999@gmail.com>
2023-05-15 17:34:33 -0700

    finalizing design of the register page

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -10,7 +10,7 @@ import mainLogo from'./resourcefiles/finstaglam_logo.png';
         const [password, setPassword] = useState('');
         const [name, setName] = useState('');
 
-           const handleSubmit = (e) => {
+           const handleRegister = (e) => {
                e.preventDefault();
                console.log(email)
                console.log(password)
@@ -25,26 +25,51 @@ import mainLogo from'./resourcefiles/finstaglam_logo.png';
 
 
         return(
-            <div className="main-container">
-                <div className="auth-form-container">
-                    <h2>Register a new account!</h2>
-                    <form className="register-form" onSubmit={handleSubmit}>
-                        <label htmlFor="name">Full Name</label>
-                        <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Enter your full name" id="name" name="name"/>
+//            <div className="main-container">
+//                <div className="auth-form-container">
+//                    <h2>Register a new account!</h2>
+//                    <form className="register-form" onSubmit={handleSubmit}>
+//                        <label htmlFor="name">Full Name</label>
+//                        <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Enter your full name" id="name" name="name"/>
+//
+//                        <label htmlFor="email">Email</label>
+//                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email id" id="email" name="email"/>
+//
+//                        <label htmlFor="password">Set Password</label>
+//                        <input value={password} onChange={({target: {value}}) => setPassword(value)} type="password" placeholder="********" id="password" name="password"/>
+//
+//                        <button> Register </button>
+//                    </form>
+//
+//                    <button className="link-btn" onClick={() => props.onFormSwitch('login')}> Already have account? The login Dammit! </button>
+//
+//                </div>
+//            </div>
 
-                        <label htmlFor="email">Email</label>
-                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email id" id="email" name="email"/>
+            <div id="wrapper">
+                      <div class="main-content">
+                        <div class="header">
+                          <img src="https://i.imgur.com/zqpwkLQ.png" />
+                        </div>
+                        <div class="l-part">
+                          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email ID" class="input-1" />
+                          <input type="text" value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Full Name" class="input-1" />
+                          <input type="text" value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Username" class="input-1" />
+                          <div class="overlap-text">
+                            <input type="password" value={password} onChange={({target: {value}}) => setPassword(value)} placeholder="Password" class="input-2" />
 
-                        <label htmlFor="password">Set Password</label>
-                        <input value={password} onChange={({target: {value}}) => setPassword(value)} type="password" placeholder="********" id="password" name="password"/>
+                          </div>
+                          <input type="button" onClick={handleRegister} value="Sign up" class="btn" />
+                        </div>
+                      </div>
+                      <div class="sub-content">
+                        <div class="s-part">
+                          Already have an account?<a href="#" onClick={() => props.onFormSwitch('login')}>Log in</a>
+                        </div>
+                      </div>
+                    </div>
 
-                        <button> Register </button>
-                    </form>
 
-                    <button className="link-btn" onClick={() => props.onFormSwitch('login')}> Already have account? The login Dammit! </button>
-
-                </div>
-            </div>
         )
 
     }
